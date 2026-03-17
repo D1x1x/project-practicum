39f5a8983a894a704202fafb7ce08a2baa5df66c
D1x1x <zavtragdestois@gmail.com>
2026-03-18 00:09:34 +0400

    feat: add base menu + setting piplane

--- src/main/java/ru/tbank/practicum/TpracticeApplication.java
+++ src/main/java/ru/tbank/practicum/TpracticeApplication.java
@@ -1,4 +1,4 @@
-package ru.tbank.practicum;
+package ru.tbank.practicum;//практикум
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
